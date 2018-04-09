00fae3ab5a0da04071b2bf6aeefa80a5115456f6
Cristiano Almeida <cs.almeida@protonmail.ch>
2018-04-09 11:06:55 +0100

    Created guessing game project with Cargo.

---
+++ guessing_game/target/debug/deps/guessing_game-a490c4e3cc19053d.d
@@ -0,0 +1,5 @@
+/Users/cristianoalmeida/Projects/learning-rust/guessing_game/target/debug/deps/guessing_game-a490c4e3cc19053d: src/main.rs
+
+/Users/cristianoalmeida/Projects/learning-rust/guessing_game/target/debug/deps/guessing_game-a490c4e3cc19053d.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game-a490c4e3cc19053d.d
@@ -0,0 +1,1 @@
+/Users/cristianoalmeida/Projects/learning-rust/guessing_game/target/debug/guessing_game-a490c4e3cc19053d.dSYM: /Users/cristianoalmeida/Projects/learning-rust/guessing_game/src/main.rs
