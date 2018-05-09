104973705e1225220b7918fc80001da2936faee8
Cristiano Almeida <cs.almeida@protonmail.ch>
2018-05-09 11:20:01 +0100

    Added comments to current version of the game.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,21 @@
+// To bring a library or other parts of Rust into scope a user statement needs to be written.
 use std::io;
 
 fn main() {
+    // Prints a line, in Rust this is a macro.
     println!("Guess the number!");
-
     println!("Please input your guess.");
 
+    // Creates a mutable empty string variable that acts as a place holder for a guess.
+    // Adds another variable that is not mutable (cannot be changed after assigned).
     let mut guess = String::new();
     let name = "Cristiano";
 
+    // Uses io module to allow input to take place.
+    // Makes use of placeholder variable.
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    // Prints guess.
     println!("{} you guessed: {}", name, guess);
 }
