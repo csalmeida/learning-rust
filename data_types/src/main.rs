439dc9a348ec1edb6d80bcfa2fad3240b15ba2f9
Cristiano Almeida <cs.almeida@protonmail.ch>
2018-05-10 14:47:04 +0100

    Boolean, character and tuple types.

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,8 +1,28 @@
 fn main() {
+    // Number types
+    println!("Printing number types:");
     let x = 1;
     let y: u8 = 2;
     let z = 3.1;
     let n: f32 = 4.2;
+    println!("x:{}\ny:{}\nz:{}\nn:{}\n", x,y,z,n);
 
-    println!("x:{}\ny:{}\nz:{}\nn:{}\n", x,y,z,n) 
+    // Boolean type
+    println!("Printing Boolean types:");
+    let foo = true;
+    let bar: bool = false;
+    println!("Foo is {} and bar is {}.\n", foo, bar);
+
+    // Character type
+    // This requires to be set in single quotes.
+    println!("Printing character types:");
+    let emoji_bow = '😄';
+    let emoji_horse: char = '🏇';
+    println!("Here's two character types: {}{}\n", emoji_bow,emoji_horse);
+
+    // Compound types
+    // Grouping multiple types together using tuples
+    println!("Printing compound types:");
+    let tup: (u8, i32, char, f32, bool) = (255, 32, '🙌', 20.77, true);
+    println!("Here's part of a tuple value: {}", tup.2);
 }
