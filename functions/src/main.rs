51451f278652afb909a207a3fb5f3e4f896db947
Cristiano Almeida <cs.almeida@protonmail.ch>
2018-05-18 10:41:57 +0100

    Created functions that return a value.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -7,8 +7,18 @@ fn sum(x: i32, y: i32) {
     println!("The result is {}.", x + y);
 }
 
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
 // Define functions anywhere, run on main.
 fn main() {
     a_function();
     sum(20,77);
+    println!("This function returns a value: {}", five());
+    println!("This function returns a value and allows an argument: {}", plus_one(16));
 }
