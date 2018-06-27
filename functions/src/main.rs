b13fab1c3e8a855c50550b01777755bb9b62ae83
Cristiano Almeida <cs.almeida@protonmail.ch>
2018-06-27 16:06:41 +0100

    Added comments to functions chapter.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -7,10 +7,12 @@ fn sum(x: i32, y: i32) {
     println!("The result is {}.", x + y);
 }
 
+// Explicitly returns a signed 32 bit integer.
 fn five() -> i32 {
     5
 }
 
+// Takes a signed 32 bit integer has an argument, returns i32.
 fn plus_one(x: i32) -> i32 {
     x + 1
 }
