ef458e2260a66e46c08487567594e28214f26e03
Cristiano <cs.almeida@protonmail.ch>
2023-03-05 18:24:42 +0000

    Completed temperature converter.

--- challenges/temperature_converter/src/main.rs
+++ challenges/temperature_converter/src/main.rs
@@ -18,47 +18,60 @@ fn main() {
     // Request input until a supported value has been entered.
     let mut has_supported_value: bool = false;
     while has_supported_value == false {
-      let mut convert_to_temperature = String::new();
+        let mut convert_to_temperature = String::new();
 
-      // Requests user input:
-      io::stdin().read_line(&mut convert_to_temperature)
-      .expect("Failed to read line");
+        // Requests user input:
+        io::stdin()
+            .read_line(&mut convert_to_temperature)
+            .expect("Failed to read line");
 
-      // Removes any trailing new lines in input:
-      convert_to_temperature = convert_to_temperature.trim().to_string();
+        // Removes any trailing new lines in input:
+        convert_to_temperature = convert_to_temperature.trim().to_string();
 
-      if convert_to_temperature.to_string() == "celsius" || convert_to_temperature.to_string() == "c" {
-        println!("Please enter your value in Fahrenheit:");
-        // Reads temperature entered by user.
-        io::stdin().read_line(&mut temperature)
-        .expect("Failed to read line");
-  
-        // Need to convert the input back into a number:
-        let temperature = temperature.trim().parse().unwrap();
-        println!("{} Fahrenheit is {} Celsius.", temperature, fahrenheit_to_celsius(temperature));
-        has_supported_value = true;
-      } else if convert_to_temperature == "fahrenheit" || convert_to_temperature == "f" {
-        println!("Please enter your value in Celsius:");
-        // Reads temperature entered by user.
-        io::stdin().read_line(&mut temperature)
-        .expect("Failed to read line");
-  
-        // Need to convert the input back into a number:
-        let temperature = temperature.trim().parse().unwrap();
-        println!("{} Celsius is {} Fahrenheit", temperature, celsius_to_fahrenheit(temperature));
-        has_supported_value = true;
-      } else {
-        println!("Temperature type not recognized.");
-        println!("Try 'celsius' or 'c' and 'fahrenheit' or 'f'");
-      }
+        if convert_to_temperature.to_string() == "celsius"
+            || convert_to_temperature.to_string() == "c"
+        {
+            println!("Please enter your value in Fahrenheit:");
+            // Reads temperature entered by user.
+            io::stdin()
+                .read_line(&mut temperature)
+                .expect("Failed to read line");
+
+            // Need to convert the input back into a number:
+            let temperature = temperature.trim().parse().unwrap();
+            println!(
+                "{} Fahrenheit is {} Celsius.",
+                temperature,
+                fahrenheit_to_celsius(temperature)
+            );
+            has_supported_value = true;
+        } else if convert_to_temperature == "fahrenheit" || convert_to_temperature == "f" {
+            println!("Please enter your value in Celsius:");
+            // Reads temperature entered by user.
+            io::stdin()
+                .read_line(&mut temperature)
+                .expect("Failed to read line");
+
+            // Need to convert the input back into a number:
+            let temperature = temperature.trim().parse().unwrap();
+            println!(
+                "{} Celsius is {} Fahrenheit",
+                temperature,
+                celsius_to_fahrenheit(temperature)
+            );
+            has_supported_value = true;
+        } else {
+            println!("Temperature type not recognized.");
+            println!("Try 'celsius' or 'c' and 'fahrenheit' or 'f'");
+        }
     }
     println!("Ended execution.");
 }
 
 fn fahrenheit_to_celsius(fahrenheit: i32) -> i32 {
-  return (fahrenheit - 32) * 5 / 9;
+    return (fahrenheit - 32) * 5 / 9;
 }
 
 fn celsius_to_fahrenheit(celsius: i32) -> i32 {
-  return (celsius * 9 / 5) + 32;
+    return (celsius * 9 / 5) + 32;
 }
