57a3604b50c07b230d32d59d3a7c9b8481f1f1c9
Cristiano <cs.almeida@protonmail.ch>
2023-07-01 13:15:03 +0100

    Tuple structs

--- chapters/tuple_structs/src/main.rs
+++ chapters/tuple_structs/src/main.rs
@@ -3,15 +3,19 @@ struct Color(i32, i32, i32);
 #[derive(Debug)]
 struct Point(i32, i32, i32);
 fn main() {
+    // Tuple structures can be defined do capture a number of values.
     let mut black = Color(0, 0, 0);
     let origin = Point(0, 0, 0);
 
-    let Color(mut r, g, b) = black;
+    // Tuples can be destructured to access individual values of the structure.
+    let Color(mut red, green, blue) = black;
 
-    r = 254;
+    // If mutable these can be changed.
+    // The r variable is the same as black.0 but one can be seen as more readable.
+    red = 254;
     black.0 = 123;
 
     println!("Black color: {:#?}", black);
-    println!("RGB: {}, {}, {}", r, g, b);
+    println!("RGB: {}, {}, {}", red, green, blue);
     println!("Origin point: {:#?}", origin);
 }
