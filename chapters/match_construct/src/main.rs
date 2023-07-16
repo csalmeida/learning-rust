e8a7eb59562b01bccaa264f7269b4e8a3d607fe2
Cristiano <cs.almeida@protonmail.ch>
2023-07-16 06:16:16 +0100

    Match construct

---
+++ chapters/match_construct/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {}
+
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
