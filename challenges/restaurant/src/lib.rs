5d237d20d33f300672db7a57f0da37b195085c27
Cristiano <cs.almeida@protonmail.ch>
2023-07-19 12:49:58 +0100

    Using front of house module

--- challenges/restaurant/src/lib.rs
+++ challenges/restaurant/src/lib.rs
@@ -44,12 +44,16 @@ mod back_of_house {
 
 fn deliver_order() {}
 
+use crate::front_of_house::hosting;
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Absolute path with use
+    hosting::add_to_waitlist();
 
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
