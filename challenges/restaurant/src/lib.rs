5d7c94d2bba9825295de7c65ca3cc709ffc4310d
Cristiano <cs.almeida@protonmail.ch>
2023-07-19 12:50:09 +0100

    Modules in multiple files

--- challenges/restaurant/src/lib.rs
+++ challenges/restaurant/src/lib.rs
@@ -1,18 +1,5 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
+pub use crate::front_of_house::hosting;
 
 mod back_of_house {
     pub enum Appetizer {
@@ -44,7 +31,6 @@ mod back_of_house {
 
 fn deliver_order() {}
 
-use crate::front_of_house::hosting;
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
